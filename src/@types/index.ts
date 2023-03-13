86e21a341313870d9dd9ac10eedc25d7be17415c
funny-family <maksvadik228@gmail.com>
2023-03-13 22:52:38 +0700

    update

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -58,3 +58,91 @@ export type UnionToArray<T, A extends unknown[] = []> = IsUnion<T> extends true
 // export type ExtractBoundEventHandler<T extends Array<any>> = UnionToArray<
 //   NonNullable<T>
 // >[1];
+
+// export type UnionToIntersection<U> = (
+//   U extends any ? (k: U) => void : never
+// ) extends (k: infer I) => void
+//   ? I
+//   : never;
+
+type InArray<T, X> = T extends readonly [X, ...infer _Rest]
+  ? true
+  : T extends readonly [X]
+  ? true
+  : T extends readonly [infer _, ...infer Rest]
+  ? InArray<Rest, X>
+  : false;
+
+/**
+ * @see https://stackoverflow.com/questions/57016728/is-there-a-way-to-define-type-for-array-with-unique-items-in-typescript
+ */
+export type UniqueArray<T> = T extends readonly [infer X, ...infer Rest]
+  ? InArray<Rest, X> extends true
+    ? ['Encountered value with duplicates:', X]
+    : readonly [X, ...UniqueArray<Rest>]
+  : T;
+
+/**
+ * @see https://stackoverflow.com/questions/53501721/typescript-exclude-property-key-when-starts-with-target-string
+ *
+ * @description
+ * Exclude Property key when starts with target string
+ *
+ * @example
+ * type Origin = {
+ *    a: string;
+ *    b: string;
+ *    _c: string;
+ *    _d: string;
+ * };
+ *
+ * type Result = FilterStartsWith<Origin, '_'>; // { _c: string; _d: string; }
+ */
+export type FilterStartsWith<
+  Set,
+  Needle extends string
+> = Set extends `${Needle}${infer _X}` ? Set : never;
+
+/**
+ * @see https://stackoverflow.com/questions/53501721/typescript-exclude-property-key-when-starts-with-target-string
+ *
+ * @description
+ * Exclude Property key when starts with target string
+ *
+ * @example
+ * type Origin = {
+ *    a: string;
+ *    b: string;
+ *    _c: string;
+ *    _d: string;
+ * };
+ *
+ * type Result = FilterNotStartingWith<Origin, '_'>; // { a: string; b: string; }
+ */
+export type FilterNotStartingWith<
+  Set,
+  Needle extends string
+> = Set extends `${Needle}${infer _X}` ? never : Set;
+
+/**
+ * @see https://dev.to/vborodulin/ts-how-to-override-properties-with-type-intersection-554l
+ *
+ * @description
+ * Override properties with type intersection
+ *
+ * @example
+ * interface A {
+ *   a: string;
+ *   b: string;
+ * }
+ *
+ * interface AB extends A {
+ *   b: number;
+ * }
+ * type Override<T1, T2> = Omit<T1, keyof T2> & T2;
+ *
+ * type AB = Override<A, { b: number }>
+ */
+export type Override<T1, T2> = Omit<T1, keyof T2> & T2;
+
+export type KeysOf<T> = Array<keyof T>;
