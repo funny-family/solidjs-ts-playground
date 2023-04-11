27e83f1f53f7c3afdbcd9679beb68e4e2841d4de
funny-family <maksvadik228@gmail.com>
2023-04-11 16:55:26 +0700

    add separate file for types of "text-field" component

---
+++ src/components/text-field/text-field.types.ts
@@ -0,0 +1,63 @@
+import type { Component, JSX } from 'solid-js';
+
+type TextFieldRootElement = HTMLDivElement;
+
+// export type TextFieldForwardElement = HTMLInputElement;
+
+type TextFieldAttrs = Omit<
+  JSX.HTMLElementTags['div'],
+  | 'children'
+  | 'innerHTML'
+  | 'innerText'
+  | 'textContent'
+  | 'contentEditable'
+  | 'contenteditable'
+>;
+
+type TextFieldProps = {
+  label?: Omit<
+    JSX.HTMLElementTags['label'],
+    /* ----------------- omitted attrs ----------------- */
+    'innerHTML' | 'innerText' | 'textContent' | 'for'
+    /* ----------------- omitted attrs ----------------- */
+  >;
+  input?: Omit<
+    JSX.HTMLElementTags['input'],
+    /* ----------------- omitted attrs ----------------- */
+    | 'innerHTML'
+    | 'innerText'
+    | 'textContent'
+    | 'children'
+    | 'accept'
+    | 'checked'
+    | 'height'
+    | 'width'
+    | 'list'
+    | 'src'
+    /* ----------------- omitted attrs ----------------- */
+    /* ----------------- overwritten attrs ----------------- */
+    | 'type'
+  > & {
+    /**
+     * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#input_types
+     */
+    type?:
+      | 'email'
+      | 'password'
+      | 'number'
+      | 'password'
+      | 'search'
+      | 'tel'
+      | 'text'
+      | 'url';
+  };
+  /* ----------------- overwritten attrs ----------------- */
+};
+
+type TextFieldCustomAttrs = JSX.CustomAttributes<TextFieldRootElement>;
+
+export type TextFieldAttrsAndProps = TextFieldAttrs &
+  TextFieldCustomAttrs &
+  TextFieldProps;
+
+export type TextFieldComponent = Component<TextFieldAttrsAndProps>;
