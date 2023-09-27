53e1d4e8e909f0548e2a6c23b8ed7054ff5113c6
funny-family <maksvadik228@gmail.com>
2023-09-27 23:09:53 +0700

    update

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,5 +1,14 @@
-import { Index, type Component, type JSX, For, mapArray } from 'solid-js';
-import { createStore } from 'solid-js/store';
+import { createSign } from 'crypto';
+import {
+  Index,
+  type Component,
+  type JSX,
+  For,
+  mapArray,
+  createSignal,
+  onMount,
+} from 'solid-js';
+import { createMutable, createStore } from 'solid-js/store';
 import { useNames } from '~/store/names.store';
 
 type AboutAttrs = JSX.CustomAttributes<HTMLDivElement>;
@@ -11,6 +20,8 @@ type AboutComponent = Component<AboutAttrsAndProps>;
 const About: AboutComponent = (attrsAndProps) => {
   const { name, changeNameToSally } = useNames();
 
+  let listRef: HTMLUListElement | null = null;
+
   const namesMap = new Map([
     [0, 'James'],
     [1, 'Robert'],
@@ -18,7 +29,30 @@ const About: AboutComponent = (attrsAndProps) => {
     [3, 'Michael'],
   ]);
 
-  const [getNamesMap, setNamesMap] = createStore(Array.from(namesMap));
+  const namesMapNodes = Array<JSX.Element>(namesMap.size);
+  const namesMapNodes_mutable = createMutable(
+    Array<JSX.Element>(namesMap.size)
+  );
+
+  onMount(() => {
+    namesMap.forEach((value, key, map) => {
+      // namesMapNodes.push(<li>{value}</li>);
+      // namesMapNodes_mutable.push(<li>{value}</li>);
+      listRef!.appendChild((<li>{value}</li>) as Node);
+    });
+  });
+
+  window.namesMap = namesMap;
+
+  // const [namesMapAs_signal_get, namesMapAs_signal_set] = createSignal(
+  //   namesMap,
+  //   {
+  //     equals: false,
+  //   }
+  // );
+
+  // const mutableNamesMap = createMutable(Array.from(namesMap));
+  // const [getNamesMap, setNamesMap] = createSignal(Array.from(namesMap));
 
   return (
     <div
@@ -31,17 +65,36 @@ const About: AboutComponent = (attrsAndProps) => {
 
         <button
           type="button"
-          onClick={() => namesMap.set(namesMap.size, '111')}
+          onClick={() => {
+            // var v = crypto.randomUUID();
+            // var ms = namesMap.size;
+            // namesMap.set(ms, v);
+            // // setNamesMap(namesMap.size, [namesMap.get(ms - 1), v]);
+            // // setNamesMap(Array.from(namesMap));
+            // mutableNamesMap[ms - 1] = [ms - 1, v];
+            console.log(listRef);
+
+            const key = namesMap.size + 1;
+            const value = crypto.randomUUID();
+            namesMap.set(key + 1, value);
+            listRef!.appendChild((<li>{value}</li>) as Node);
+
+            // namesMapAs_signal_set(namesMap);
+          }}
         >
           add name
         </button>
 
         <section>
           <h1>Names</h1>
-          <ul>
-            <For each={getNamesMap}>
+          <ul ref={listRef}>
+            {/* <For each={Array.from(namesMapAs_signal_get())}>
               {({ 0: key, 1: value }) => <li>{value}</li>}
-            </For>
+            </For> */}
+
+            {namesMapNodes}
+            {/* {namesMapNodes_mutable} */}
+            {/* {namesMap.values()} */}
           </ul>
         </section>
 
