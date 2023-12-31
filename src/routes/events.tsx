2c92d8f4f0c3d4c6b1bde46a6cde58c7963f6e85
funny-family <maksvadik228@gmail.com>
2023-12-31 20:47:45 +0700

    update

---
+++ src/routes/events.tsx
@@ -0,0 +1,33 @@
+import { createSignal } from 'solid-js';
+
+const Events = () => {
+  const [count, setCount] = createSignal(0);
+
+  return (
+    <div>
+      <div>count: {count()}</div>
+
+      <div>
+        <div>"change" event test</div>
+        <div
+          onChange={(event) => {
+            console.log('count before:', count());
+            console.log('"change" root event:', event);
+
+            setCount((value) => value + 1);exit
+            console.log('count after:', count());
+          }}
+        >
+          <input
+            type="text"
+            // onChange={(event) => {
+            //   console.log('"change" input event:', event);
+            // }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Events;
