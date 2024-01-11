451697ba5bd2e29484cdb17dfc48dc9b3991dc11
funny-family <maksvadik228@gmail.com>
2024-01-11 19:40:32 +0700

    update

--- src/routes/events.tsx
+++ src/routes/events.tsx
@@ -5,26 +5,37 @@ const Events = () => {
 
   return (
     <div>
-      <div>count: {count()}</div>
-
       <div>
-        <div>"change" event test</div>
-        <div
-          onChange={(event) => {
-            console.log('count before:', count());
-            console.log('"change" root event:', event);
+        <div>count: {count()}</div>
+        <div>
+          <div>"change" event test</div>
+          <div
+            onChange={(event) => {
+              console.log('count before:', count());
+              console.log('"change" root event:', event);
 
-            setCount((value) => value + 1);
-            console.log('count after:', count());
-          }}
-        >
-          <input
-            type="text"
-            // onChange={(event) => {
-            //   console.log('"change" input event:', event);
-            // }}
-          />
+              setCount((value) => value + 1);
+              console.log('count after:', count());
+            }}
+          >
+            <input
+              type="text"
+              // onChange={(event) => {
+              //   console.log('"change" input event:', event);
+              // }}
+            />
+          </div>
         </div>
+      </div>
+
+      <hr />
+
+      <div
+        onclick={(event) => {
+          console.log('"oninput" event:', event);
+        }}
+      >
+        inline click
       </div>
     </div>
   );
