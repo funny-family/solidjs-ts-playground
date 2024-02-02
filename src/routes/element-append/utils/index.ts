f7fbcf800c296ada5529f71ec0426b4c5277c881
funny-family <maksvadik228@gmail.com>
2024-02-02 19:38:38 +0700

    add utils

---
+++ src/routes/element-append/utils/index.ts
@@ -0,0 +1,15 @@
+// const getForwardElement = (() => {
+//   const element = resolvedChildren().querySelector(
+//     `[${dataIsForwardElementAttrName}]`
+//   );
+
+//   if (element == null) {
+//     throw new Error(
+//       `"${dataIsForwardElementAttrName}" attribute is required!`
+//     );
+//   }
+
+//   return element;
+// })();
+// const 
+//  getForwardElement = () =>
