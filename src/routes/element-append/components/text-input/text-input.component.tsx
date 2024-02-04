044542544998c22899f1615f608b842eabeb90f6
funny-family <maksvadik228@gmail.com>
2024-02-04 13:56:47 +0700

    add "text-input" component

--- src/routes/element-append/components/text-input/text-input.component.tsx
+++ src/routes/element-append/components/text-input/text-input.component.tsx
@@ -1,12 +1,13 @@
 import { type JSX } from 'solid-js';
 import styles from './text-input.module.css';
+import inputStyles from '../../styles/input.module.css';
 
 export const TextInput = (props: JSX.IntrinsicElements['input']) => {
   return (
     <input
       {...props}
       type={props?.type || 'text'}
-      class={`${props?.class || ''} ${styles.textInput}`}
+      class={`${props?.class || ''} ${styles.textInput} ${inputStyles.input}`}
       data-is-forward-element
     />
   );
