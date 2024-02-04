790eb715c92186765e8c57a0d72451d25d2dc5be
funny-family <maksvadik228@gmail.com>
2024-02-04 11:21:19 +0700

    add "input-container" component

--- src/routes/element-append/components/input-container/input-container.component.tsx
+++ src/routes/element-append/components/input-container/input-container.component.tsx
@@ -1,8 +1,27 @@
-import { type JSX } from 'solid-js';
+import { type JSX, splitProps } from 'solid-js';
 import styles from './input-container.module.css';
+import inputStyles from '../../styles/input.module.css';
 
-export const InputContainer = (props: JSX.IntrinsicElements['div']) => {
+export const dataIsInputContainerAttrName = 'data-is-input-container' as const;
+export const dataIsInputInnerContainerAttrName =
+  'data-is-input-inner-container' as const;
+export const dataIsForwardElementAttrName = 'data-is-forward-element' as const;
+
+export const InputContainer = (attrsAndProps: JSX.IntrinsicElements['div']) => {
+  const [children, resetAttrs] = splitProps(attrsAndProps, ['children']);
+
   return (
-    <div {...props} class={`${props?.class || ''} ${styles.inputContainer}`} />
+    <div
+      {...resetAttrs}
+      class={`${resetAttrs?.class || ''} ${styles.inputContainer}`}
+      data-is-input-container
+    >
+      <div
+        class={`${styles.innerContainer} ${inputStyles.inputInnerContainer}`}
+        data-is-input-inner-container
+      >
+        {children.children}
+      </div>
+    </div>
   );
 };
