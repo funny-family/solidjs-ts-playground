06c9de679bfc40cea7538e7464c41dcd8895716f
funny-family <maksvadik228@gmail.com>
2024-02-04 13:56:31 +0700

    add "seclet-input" component

--- src/routes/element-append/components/select-input/select-input.component.tsx
+++ src/routes/element-append/components/select-input/select-input.component.tsx
@@ -1,11 +1,14 @@
 import { type JSX } from 'solid-js';
 import styles from './select-input.module.css';
+import inputStyles from '../../styles/input.module.css';
 
 export const SelectInput = (attrsAndProps: JSX.IntrinsicElements['select']) => {
   return (
     <select
       {...attrsAndProps}
-      class={`${attrsAndProps?.class || ''} ${styles.select}`}
+      class={`${attrsAndProps?.class || ''} ${styles.select} ${
+        inputStyles.input
+      }`}
       data-is-forward-element
     />
   );
