f833364d1045fdd4e8914fbaf83e25dad14c57a3
funny-family <maksvadik228@gmail.com>
2024-02-07 23:52:51 +0700

    update

---
+++ src/hooks/form/form.ts
@@ -0,0 +1,93 @@
+type F = {
+  name: string;
+  ref: (el: HTMLElement) => void;
+  onBlur: (
+    event: Event & {
+      currentTarget: HTMLElement;
+      target: Element;
+    }
+  ) => void;
+  onChange: (
+    event: Event & {
+      currentTarget: HTMLElement;
+      target: Element;
+    }
+  ) => void;
+};
+
+export var createForm = () => {
+  var fieldPropsMap = new Map<string, F>();
+
+  const register = (name: string) => {
+    const field: F = {
+      name,
+      ref: (el) => {
+        //
+      },
+      onBlur: (event) => {
+        //
+      },
+      onChange: (event) => {
+        //
+      },
+    };
+
+    fieldPropsMap.set(name, field);
+
+    return field;
+  };
+
+  const unregister = (name: string) => {
+    fieldPropsMap.delete(name);
+  };
+
+  const submit = (
+    event: Event & {
+      currentTarget: HTMLFormElement;
+      target: Element;
+    }
+  ) => {
+    return async (
+      onSubmit: (
+        event: Event & {
+          currentTarget: HTMLFormElement;
+          target: Element;
+        }
+      ) => void | Promise<void>
+    ) => {
+      try {
+        await onSubmit(event);
+      } catch (error) {
+        //
+      } finally {
+        //
+      }
+    };
+  };
+
+  return {
+    register,
+    unregister,
+    submit,
+    fieldPropsMap,
+  };
+};
+
+/* TODO: add event system
+
+  on('submit-start', () => {
+    //...
+  });
+
+  on('submit-end', () => {
+    //...
+  });
+
+  on('submit-success', () => {
+    //...
+  });
+
+  on('submit-unsuccess', () => {
+    //...
+  });
+*/
