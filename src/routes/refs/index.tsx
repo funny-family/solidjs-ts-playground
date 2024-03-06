062ae512ffc5dea0b2a8586e76f2b40d3f1a88d0
funny-family <maksvadik228@gmail.com>
2024-03-06 19:27:51 +0700

    update

---
+++ src/routes/refs/index.tsx
@@ -0,0 +1,50 @@
+import { onMount } from 'solid-js';
+
+const Input = (attrsAndProps: any) => {
+  var ref: HTMLInputElement = attrsAndProps?.ref as any;
+
+  onMount(() => {
+    console.log({ ref });
+  });
+
+  return (
+    <>
+      <input
+        {...attrsAndProps}
+        type={attrsAndProps?.type || 'text'}
+        ref={(el) => {
+          ref = el;
+        }}
+      />
+    </>
+  );
+};
+
+const Refs = () => {
+  var inputRef: HTMLInputElement = null as any;
+
+  onMount(() => {
+    inputRef.focus();
+  });
+
+  var DefaultChildren = () => {
+    return (
+      <>
+        <Input
+          ref={(el: any) => {
+            inputRef = el;
+          }}
+          // ref={inputRef}
+        />
+      </>
+    );
+  };
+
+  return (
+    <div>
+      <DefaultChildren />
+    </div>
+  );
+};
+
+export default Refs;
