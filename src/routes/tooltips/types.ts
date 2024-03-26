c11f3cf5e5cf3d2d35d018f0a253ea79a1bf80f4
funny-family <maksvadik228@gmail.com>
2024-03-26 11:54:13 +0700

    add types

---
+++ src/routes/tooltips/types.ts
@@ -0,0 +1,34 @@
+import type { ChildrenReturn, JSX } from 'solid-js';
+
+export type TooltipPosition =
+  | 'top-left-corner'
+  | 'top-left'
+  | 'top-center'
+  | 'top-right'
+  | 'top-right-corner'
+  | 'right-top'
+  | 'right-center'
+  | 'right-bottom'
+  | 'bottom-right-corner'
+  | 'bottom-right'
+  | 'bottom-center'
+  | 'bottom-left'
+  | 'bottom-left-corner'
+  | 'left-bottom'
+  | 'left-center'
+  | 'left-top';
+
+export type WithResolvedChildren<TRecord extends any> = TRecord & {
+  toArray: () => HTMLElement[];
+};
+
+export type TooltipDirectiveElementArg = HTMLElement;
+
+export type TooltipDirectiveAccessorArg = () => HTMLElement | HTMLElement[];
+
+export type TooltipDirectiveFunction = (
+  element: TooltipDirectiveElementArg,
+  accessor: TooltipDirectiveElementArg
+) => void;
+
+export type TooltipDirective = () => void;
