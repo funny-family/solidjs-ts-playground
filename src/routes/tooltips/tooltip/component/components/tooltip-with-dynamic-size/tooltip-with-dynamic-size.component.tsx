027d8f551d8e05d515351457a6c3661f17024e5c
funny-family <maksvadik228@gmail.com>
2024-05-28 12:40:25 +0700

    update

--- src/routes/tooltips/tooltip/component/components/tooltip-with-dynamic-size/tooltip-with-dynamic-size.component.tsx
+++ src/routes/tooltips/tooltip/component/components/tooltip-with-dynamic-size/tooltip-with-dynamic-size.component.tsx
@@ -35,56 +35,20 @@ export var TooltipWithDynamicSize: TooltipWithDynamicSizeComponent = (
     return props.children;
   }) as unknown as () => HTMLElement;
 
-  // var onTransitionEnd = (event: TransitionEvent) => {
-  //   console.group('"transitionend"');
-  //   console.log('event:', event);
-  //   console.log('target:', event.target);
-  //   console.log(
-  //     'elementRect:',
-  //     (event.target as Element).getBoundingClientRect()
-  //   );
-  //   console.groupEnd();
-  // };
+  var onTransitionEnd = (event: TransitionEvent) => {
+    console.group('"transitionend"');
+    console.log('event:', event);
+    console.log('target:', event.target);
+    console.log(
+      'elementRect:',
+      (event.target as Element).getBoundingClientRect()
+    );
+    console.groupEnd();
+  };
 
   // children().addEventListener('transitionend', onTransitionEnd);
 
-  createEffect(() => {
-    // console.group('RenderEffect:');
-    // console.log('children: ', children());
-    // console.log(
-    //   'isElementOverflowingBody: ',
-    //   isElementOverflowingBody(children() as HTMLElement)
-    // );
-    // console.log('body rect:', document.body.getBoundingClientRect());
-    // console.log('element rect:', (children() as any).getBoundingClientRect());
-    // console.groupEnd();
-
-    console.log({
-      c: children(),
-      r: children().getBoundingClientRect(),
-      p: children().parentElement,
-      dataId: children().dataset.id,
-      // t: document
-      //   .querySelector(`[data-id=${children().dataset.id}]`)!
-      //   .getBoundingClientRect(),
-    });
-
-    // setTimeout(() => {
-    //   //
-    // }, 50);
-  });
-
-  // onCleanup(() => {
-  //   children().removeEventListener('transitionend', onTransitionEnd);
-
-  //   console.log('"TooltipWithDynamicSize" cleanup');
-  // });
-
-  // createEffect(
-  //   on(children, (element) => {
-  //     console.log(123132131, element, element.getBoundingClientRect());
-  //   })
-  // );
+  console.log(1231231, children());
 
   return children;
 };
