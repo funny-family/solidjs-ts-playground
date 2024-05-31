2410e123d1e3157aed407a460af10d7577e9cb19
funny-family <maksvadik228@gmail.com>
2024-05-31 11:01:04 +0700

    update

--- src/routes/tooltips/index.tsx
+++ src/routes/tooltips/index.tsx
@@ -26,119 +26,138 @@ var Tooltips = () => {
 
   var [isTooltipVisible, setTooltipVisibility] = createSignal(false);
 
+  var [isSectionVisible, setSectionVisibility] = createSignal(true);
+
   onMount(() => {
     window.scrollTo(0, 100);
   });
 
   return (
     <main style={{ 'margin-inline': '2em' }}>
-      <section>
-        <h1
-        // use:test={1}
-        >
-          What is Lorem Ipsum?
-        </h1>
-        <p>
-          <b>Lorem Ipsum</b> is simply dummy text of the printing and
-          typesetting industry. Lorem Ipsum has been the industry's standard
-          dummy text ever since the 1500s, when an unknown printer took a galley
-          of type and scrambled it to make a type specimen book. It has survived
-          not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in
-          the{' '}
-          <b
-            style={{ border: '1px solid blue' }}
-            tabIndex={0}
-            onMouseEnter={() => {
-              setTooltipVisibility(true);
-            }}
-            onMouseLeave={() => {
-              setTooltipVisibility(false);
-            }}
-            // use:tooltip={
-            //   <Show when={isTooltipVisible()}>
-            //     <TooltipWithDynamicSize>
-            //       <Tooltip
-            //         class="custom-tooltip solid-js-tooltip-position__top-center"
-            //         data-is-tooltip={false}
-            //       >
-            //         just tooltip!
-            //       </Tooltip>
-            //     </TooltipWithDynamicSize>
-            //   </Show>
-            // }
+      <button
+        type="button"
+        onClick={() => {
+          setSectionVisibility((value) => {
+            return !value;
+          });
+        }}
+      >
+        )))))
+      </button>
 
-            // use:tooltip={
-            //   <Transition name="slide-fade">
-            //     <Show when={isTooltipVisible()}>
-            //       <Tooltip
-            //         class="custom-tooltip solid-js-tooltip-position__top-center"
-            //         // class="custom-tooltip"
-            //         data-is-tooltip={false}
-            //       >
-            //         just tooltip!
-            //       </Tooltip>
-            //     </Show>
-            //   </Transition>
-            // }
-
-            // use:tooltip={
-            //   <>
-            //     <Show when={isTooltipVisible()}>
-            //       <Tooltip
-            //         id="sjnf67___1"
-            //         class="custom-tooltip"
-            //         data-is-tooltip={false}
-            //       >
-            //         just tooltip!
-            //       </Tooltip>
-            //     </Show>
-            //     <Transition name="slide-fade">
-            //       <Show when={isTooltipVisible()}>
-            //         <Tooltip id="sjnf67___2" class="custom-tooltip">
-            //           slide fade tooltip
-            //         </Tooltip>
-            //       </Show>
-            //     </Transition>
-            //   </>
-            // }
-
-            use:tooltip={
-              <TransitionGroup name="slide-fade">
-                <Show when={isTooltipVisible()}>
-                  <For
-                    each={[
-                      <TooltipWithDynamicSize>
-                        <Tooltip class="custom-tooltip solid-js-tooltip-position__top-center">
-                          just tooltip!
-                        </Tooltip>
-                      </TooltipWithDynamicSize>,
-                      <TooltipWithDynamicSize>
-                        <Tooltip
-                          style={{ width: '300px' }}
-                          class="custom-tooltip solid-js-tooltip-position__bottom-right-corner"
-                        >
-                          Lorem ipsum dolor, sit amet consectetur adipisicing
-                          elit. Maxime, facilis.
-                        </Tooltip>
-                      </TooltipWithDynamicSize>,
-                    ]}
-                  >
-                    {(el) => {
-                      return el;
-                    }}
-                  </For>
-                </Show>
-              </TransitionGroup>
-            }
+      <Show when={isSectionVisible()}>
+        <section>
+          <h1
+          // use:test={1}
           >
-            1960s
-          </b>{' '}
-          with the release of Letraset sheets containing Lorem Ipsum passages,
-          and more recently with desktop publishing software like Aldus
-          PageMaker including versions of Lorem Ipsum.
-        </p>
-      </section>
+            What is Lorem Ipsum?
+          </h1>
+          <p>
+            <b>Lorem Ipsum</b> is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum has been the industry's standard
+            dummy text ever since the 1500s, when an unknown printer took a
+            galley of type and scrambled it to make a type specimen book. It has
+            survived not only five centuries, but also the leap into electronic
+            typesetting, remaining essentially unchanged. It was popularised in
+            the{' '}
+            <b
+              style={{ border: '1px solid blue' }}
+              tabIndex={0}
+              onMouseEnter={() => {
+                setTooltipVisibility(true);
+              }}
+              onMouseLeave={() => {
+                setTooltipVisibility(false);
+              }}
+              // use:tooltip={
+              //   <Show when={isTooltipVisible()}>
+              //     <TooltipWithDynamicSize>
+              //       <Tooltip
+              //         class="custom-tooltip solid-js-tooltip_default-position solid-js-tooltip-position__top-center"
+              //         data-is-tooltip={false}
+              //       >
+              //         just tooltip!
+              //       </Tooltip>
+              //     </TooltipWithDynamicSize>
+              //   </Show>
+              // }
+
+              // use:tooltip={
+              //   <Transition name="slide-fade">
+              //     <Show when={isTooltipVisible()}>
+              //       <Tooltip
+              //         class="custom-tooltip solid-js-tooltip_default-position solid-js-tooltip-position__top-center"
+              //         // class="custom-tooltip"
+              //         data-is-tooltip={false}
+              //       >
+              //         just tooltip!
+              //       </Tooltip>
+              //     </Show>
+              //   </Transition>
+              // }
+
+              // use:tooltip={
+              //   <>
+              //     <Show when={isTooltipVisible()}>
+              //       <Tooltip
+              //         id="sjnf67___1"
+              //         class="custom-tooltip"
+              //         data-is-tooltip={false}
+              //       >
+              //         just tooltip!
+              //       </Tooltip>
+              //     </Show>
+              //     <Transition name="slide-fade">
+              //       <Show when={isTooltipVisible()}>
+              //         <Tooltip id="sjnf67___2" class="custom-tooltip">
+              //           slide fade tooltip
+              //         </Tooltip>
+              //       </Show>
+              //     </Transition>
+              //   </>
+              // }
+
+              use:tooltip={
+                <TransitionGroup name="slide-fade">
+                  <Show when={isTooltipVisible()}>
+                    <For
+                      each={[
+                        <TooltipWithDynamicSize>
+                          <Tooltip
+                            class="custom-tooltip solid-js-tooltip_default-position solid-js-tooltip-position__top-center"
+                            data-has-transition-class={false}
+                          >
+                            just tooltip!
+                          </Tooltip>
+                        </TooltipWithDynamicSize>,
+                        <TooltipWithDynamicSize>
+                          <Tooltip
+                            style={{ width: '300px' }}
+                            class="custom-tooltip solid-js-tooltip_default-position solid-js-tooltip-position__bottom-right-corner"
+                            data-has-transition-class={false}
+                          >
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Maxime, facilis.
+                          </Tooltip>
+                        </TooltipWithDynamicSize>,
+                      ]}
+                    >
+                      {(el) => {
+                        return el;
+                      }}
+                    </For>
+                  </Show>
+                </TransitionGroup>
+              }
+            >
+              1960s
+            </b>{' '}
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </section>
+      </Show>
       {Array(80)
         .fill(1)
         .map((n) => {
