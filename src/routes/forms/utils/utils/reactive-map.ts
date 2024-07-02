2d163bafecb373fae33950c205c19d1002b2ee02
funny-family <maksvadik228@gmail.com>
2024-07-02 16:08:49 +0700

    add utils

---
+++ src/routes/forms/utils/utils/reactive-map.ts
@@ -0,0 +1,134 @@
+import { batch } from 'solid-js';
+import { TriggerCache } from './trigger-cache';
+
+var $KEYS = Symbol('track-keys');
+
+/**
+ * A reactive version of `Map` data structure. All the reads (like `get` or `has`) are signals, and all the writes (`delete` or `set`) will cause updates to appropriate signals.
+ * @param initial initial entries of the reactive map
+ * @param equals signal equals function, determining if a change should cause an update
+ * @see https://github.com/solidjs-community/solid-primitives/tree/main/packages/map#ReactiveMap
+ * @example
+ * const userPoints = new ReactiveMap<User, number>();
+ * createEffect(() => {
+ *    userPoints.get(user1) // => T: number | undefined (reactive)
+ *    userPoints.has(user1) // => T: boolean (reactive)
+ *    userPoints.size // => T: number (reactive)
+ * });
+ * // apply changes
+ * userPoints.set(user1, 100);
+ * userPoints.delete(user2);
+ * userPoints.set(user1, { foo: "bar" });
+ */
+export class ReactiveMap<K, V> extends Map<K, V> {
+  #keyTriggers = new TriggerCache<K | typeof $KEYS>();
+  #valueTriggers = new TriggerCache<K>();
+
+  constructor(initial?: Iterable<readonly [K, V]> | null) {
+    super();
+
+    if (initial) for (const v of initial) super.set(v[0], v[1]);
+  }
+
+  // reads
+  has(key: K): boolean {
+    this.#keyTriggers.track(key);
+
+    return super.has(key);
+  }
+  get(key: K): V | undefined {
+    this.#valueTriggers.track(key);
+
+    return super.get(key);
+  }
+  get size(): number {
+    this.#keyTriggers.track($KEYS);
+
+    return super.size;
+  }
+
+  *keys(): IterableIterator<K> {
+    for (const key of super.keys()) {
+      this.#keyTriggers.track(key);
+
+      yield key;
+    }
+
+    this.#keyTriggers.track($KEYS);
+  }
+  *values(): IterableIterator<V> {
+    for (const [key, v] of super.entries()) {
+      this.#valueTriggers.track(key);
+
+      yield v;
+    }
+
+    this.#keyTriggers.track($KEYS);
+  }
+  *entries(): IterableIterator<[K, V]> {
+    for (const entry of super.entries()) {
+      this.#valueTriggers.track(entry[0]);
+
+      yield entry;
+    }
+
+    this.#keyTriggers.track($KEYS);
+  }
+
+  // writes
+  set(key: K, value: V): this {
+    batch(() => {
+      if (super.has(key)) {
+        if (super.get(key)! === value) {
+          return;
+        }
+      } else {
+        this.#keyTriggers.dirty(key);
+        this.#keyTriggers.dirty($KEYS);
+      }
+      this.#valueTriggers.dirty(key);
+      super.set(key, value);
+    });
+
+    return this;
+  }
+  delete(key: K): boolean {
+    var r = super.delete(key);
+
+    if (r) {
+      batch(() => {
+        this.#keyTriggers.dirty(key);
+        this.#keyTriggers.dirty($KEYS);
+        this.#valueTriggers.dirty(key);
+      });
+    }
+
+    return r;
+  }
+  clear(): void {
+    if (super.size) {
+      batch(() => {
+        for (const v of super.keys()) {
+          this.#keyTriggers.dirty(v);
+          this.#valueTriggers.dirty(v);
+        }
+        super.clear();
+        this.#keyTriggers.dirty($KEYS);
+      });
+    }
+  }
+
+  // callback
+  forEach(callbackfn: (value: V, key: K, map: this) => void) {
+    this.#keyTriggers.track($KEYS);
+
+    for (const [key, v] of super.entries()) {
+      this.#valueTriggers.track(key);
+      callbackfn(v, key, this);
+    }
+  }
+
+  [Symbol.iterator](): IterableIterator<[K, V]> {
+    return this.entries();
+  }
+}
---
+++ src/routes/forms/utils/utils/trigger-cache.ts
@@ -0,0 +1,60 @@
+import {
+  DEV,
+  SignalOptions,
+  createSignal,
+  getListener,
+  onCleanup,
+} from 'solid-js';
+import { isServer } from 'solid-js/web';
+
+var triggerOptions: SignalOptions<any> =
+  !isServer && DEV ? { equals: false, name: 'trigger' } : { equals: false };
+var triggerCacheOptions: SignalOptions<any> =
+  !isServer && DEV ? { equals: false, internal: true } : triggerOptions;
+
+export class TriggerCache<T> {
+  #map: Map<
+    T,
+    {
+      // track
+      $(): void;
+      // dirty
+      $$(): void;
+      // n of listeners
+      n: number;
+    }
+  >;
+
+  constructor(mapConstructor?: WeakMapConstructor | MapConstructor) {
+    mapConstructor = Map;
+
+    this.#map = new (mapConstructor as MapConstructor)();
+  }
+
+  dirty(key: T) {
+    if (isServer) return;
+    this.#map.get(key)?.$$();
+  }
+
+  track(key: T) {
+    if (!getListener()) {
+      return;
+    }
+
+    var trigger = this.#map.get(key);
+    if (!trigger) {
+      const { 0: $, 1: $$ } = createSignal(undefined, triggerCacheOptions);
+      this.#map.set(key, (trigger = { $, $$, n: 1 }));
+    } else trigger.n++;
+
+    onCleanup(() => {
+      // remove the trigger when no one is listening to it
+      if (trigger!.n-- === 1) {
+        // microtask is to avoid removing the trigger used by a single listener
+        queueMicrotask(() => trigger!.n === 0 && this.#map.delete(key));
+      }
+    });
+
+    trigger.$();
+  }
+}
