b14a8a33bffe1e59d391cdd736a30a307a663a2d
funny-family <maksvadik228@gmail.com>
2024-07-29 10:49:16 +0700

    update

--- src/utils/reactive-map/index.ts
+++ src/utils/reactive-map/index.ts
@@ -22,32 +22,40 @@ export class ReactiveMap<K, V> extends Map<K, V> {
   }
   get(key: K): V | undefined {
     this.#valueTriggers.track(key);
+
     return super.get(key);
   }
   get size(): number {
     this.#keyTriggers.track($KEYS);
+
     return super.size;
   }
 
   *keys(): IterableIterator<K> {
     for (const key of super.keys()) {
       this.#keyTriggers.track(key);
+
       yield key;
     }
+
     this.#keyTriggers.track($KEYS);
   }
   *values(): IterableIterator<V> {
     for (const [key, v] of super.entries()) {
       this.#valueTriggers.track(key);
+
       yield v;
     }
+
     this.#keyTriggers.track($KEYS);
   }
   *entries(): IterableIterator<[K, V]> {
     for (const entry of super.entries()) {
       this.#valueTriggers.track(entry[0]);
+
       yield entry;
     }
+
     this.#keyTriggers.track($KEYS);
   }
 
@@ -55,26 +63,31 @@ export class ReactiveMap<K, V> extends Map<K, V> {
   set(key: K, value: V): this {
     batch(() => {
       if (super.has(key)) {
-        if (super.get(key)! === value) return;
+        if (super.get(key)! === value) {
+          return;
+        }
       } else {
         this.#keyTriggers.dirty(key);
         this.#keyTriggers.dirty($KEYS);
       }
+
       this.#valueTriggers.dirty(key);
       super.set(key, value);
     });
     return this;
   }
   delete(key: K): boolean {
-    const r = super.delete(key);
-    if (r) {
+    const isDeleted = super.delete(key);
+
+    if (isDeleted) {
       batch(() => {
         this.#keyTriggers.dirty(key);
         this.#keyTriggers.dirty($KEYS);
         this.#valueTriggers.dirty(key);
       });
     }
-    return r;
+
+    return isDeleted;
   }
   clear(): void {
     if (super.size) {
@@ -83,6 +96,7 @@ export class ReactiveMap<K, V> extends Map<K, V> {
           this.#keyTriggers.dirty(v);
           this.#valueTriggers.dirty(v);
         }
+
         super.clear();
         this.#keyTriggers.dirty($KEYS);
       });
@@ -92,8 +106,10 @@ export class ReactiveMap<K, V> extends Map<K, V> {
   // callback
   forEach(callbackfn: (value: V, key: K, map: this) => void) {
     this.#keyTriggers.track($KEYS);
+
     for (const [key, v] of super.entries()) {
       this.#valueTriggers.track(key);
+
       callbackfn(v, key, this);
     }
   }
