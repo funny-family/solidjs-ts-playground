41a019e3fc7c21465db3d24e717f5995153f627e
funny-family <maksvadik228@gmail.com>
2024-09-19 18:11:50 +0700

    update

--- src/routes/calculator/index.tsx
+++ src/routes/calculator/index.tsx
@@ -4,8 +4,6 @@ var src = '1 + 2 * 9';
 var lexer = new Lexer(src);
 console.log(lexer.tokenize());
 
-// console.log(lexer, lexer.next(), lexer.next(), lexer.unnext());
-
 export default () => {
   return <div>index</div>;
 };
