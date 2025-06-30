a1c59075e7f65692d9bb047acfec94bf153bcf5a
funny-family <maksvadik228@gmail.com>
2025-06-30 13:23:35 +0700

    update

--- src/routes/timers/timers/plugins/types.ts
+++ src/routes/timers/timers/plugins/types.ts
@@ -4,8 +4,20 @@ type PickEntry<T extends [any, any]> = {
   [Key in T[0]]: [Key, any];
 }[T[0]];
 
-export type PluginFunction<TEntry extends MapEntries> = <
-  TRecordMapEntry extends MapEntries
+// export type PluginFunction<TEntry extends MapEntries> = <
+//   TRecordMapEntry extends MapEntries
+// >(
+//   recordMap: DependentMap<TRecordMapEntry>
+// ) => DependentMap<TEntry | Exclude<TRecordMapEntry, PickEntry<TEntry>>>;
+
+// export type PluginFunction<TNew extends MapEntries> = <
+//   TBase extends MapEntries
+// >(
+//   recordMap: DependentMap<TBase>
+// ) => DependentMap<Exclude<TBase, PickEntry<TNew>> | TNew>;
+
+export type PluginFunction<TNew extends MapEntries> = <
+  TBase extends MapEntries
 >(
-  recordMap: DependentMap<TRecordMapEntry>
-) => DependentMap<TEntry | Exclude<TRecordMapEntry, PickEntry<TEntry>>>;
+  recordMap: DependentMap<TBase>
+) => DependentMap<TNew | Exclude<TBase, PickEntry<TNew>>>;
