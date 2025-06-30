3a263d2595b3f412a38ef460cdb97f264f4c2956
funny-family <maksvadik228@gmail.com>
2025-06-30 13:23:09 +0700

    update

--- src/routes/timers/timers/timer/plugins/with-laps/with-laps.plugin.ts
+++ src/routes/timers/timers/timer/plugins/with-laps/with-laps.plugin.ts
@@ -3,7 +3,7 @@ import type { PluginFunction } from '../../../plugins/types';
 import type { DependentMap } from '../../../types';
 import { setup_withLaps } from '../../../plugins/setup-with-laps/setup-with-laps';
 
-export const withLaps = ((
+export const withLaps: PluginFunction<BaseEntry | WithLapsRecordEntry> = (
   recordMap: DependentMap<BaseEntry | WithLapsRecordEntry>
 ) => {
   var lapsControl = setup_withLaps();
@@ -11,9 +11,9 @@ export const withLaps = ((
   const plugin = lapsControl.plugin;
 
   const milliseconds = recordMap.get('milliseconds')!;
-  const addLap = setup_addLap(Number(milliseconds()));
+  const addLap = setup_addLap(() => new Number(milliseconds()));
 
   recordMap.set('addLap', addLap);
 
-return plugin(recordMap);
-}) as PluginFunction<WithLapsRecordEntry>;
+  return plugin(recordMap);
+};
